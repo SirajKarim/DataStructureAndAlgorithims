7fb969eb97bde22d96869f0fb3b1f4d95b2ff941
SirajKarim <msirajakarim437@gmail.com>
2020-04-10 18:18:34 +0500

    working with generic variable

---
+++ Lab3Generic/Lab3Generic/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab3Generic
+{
+    class Program
+    {
+        //class MyClass <y>
+        //{
+        //    //public void print<t>(t value)
+        //    //{
+        //    //    Console.WriteLine(value);
+        //    //}
+        //}
+        class class1<t>
+        {
+            public t[] arr;
+            int index;
+            public void Add(t value)
+            {
+                arr[index] = value;
+                index++;
+            }
+            public void print()
+            {
+                foreach (var item in arr)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            //MyClass<int> p = new MyClass<int>();
+            //p.print<string>("Hello World");
+            //methodPrint<string,int,int>("Muhammad Siraj",2075421,3536421);
+            class1<int> c = new class1<int>();
+            c.arr=new int[5];
+            c.Add(5);
+            c.Add(6);
+            c.Add(7);
+            c.Add(9);
+            c.print();
+            
+        }
+        //public static void methodPrint<t,z,y>(t val,z value,y val1)
+        //{
+        //    Console.WriteLine("{0},{1},{2}",val,value,val1);
+        //    Console.WriteLine(value);
+
+        //}
+    }
+}
