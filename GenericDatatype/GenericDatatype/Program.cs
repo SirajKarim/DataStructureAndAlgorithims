bfb3c61a3d56805ede2e80255e6286f699f886b8
SirajKarim <msirajakarim437@gmail.com>
2020-04-11 09:08:48 +0500

    working with generic

---
+++ GenericDatatype/GenericDatatype/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDatatype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass();
+            obj.swap<int>(59, 5);
+        }
+    }
+    class myClass
+    {
+        public void swap<t>(t a, t b)
+        {
+            t c = a;
+            a = b;
+            b = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
