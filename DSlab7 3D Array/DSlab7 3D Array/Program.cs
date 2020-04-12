5087a9867882b166d9d8204b28a16272c8ae95bf
SirajKarim <msirajakarim437@gmail.com>
2020-04-12 15:22:00 +0500

    Working with 3d array

---
+++ DSlab7 3D Array/DSlab7 3D Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSlab7_3D_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[, ,] arr = new int[2,2,2] { {{1,3},{2,4}},{{3,6},{4,8}}};
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    for (int k = 0; k < arr.GetLength(2); k++)
+                    {
+                        Console.Write(" {0} ",arr[i,k,j]);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
