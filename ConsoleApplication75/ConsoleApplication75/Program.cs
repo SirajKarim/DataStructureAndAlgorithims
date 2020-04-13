4c0823918a64850cd0c668a576224b00e34382ac
SirajKarim <msirajakarim437@gmail.com>
2020-04-13 12:12:07 +0500

    practice

---
+++ ConsoleApplication75/ConsoleApplication75/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApplication75
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CQueue cq = new CQueue();
+            cq.Enqueue(1);
+            cq.Enqueue(2);
+            cq.Enqueue(3);
+            cq.Dequeue();
+            Console.WriteLine(cq.Peekfront());
+            
+        }
+    }
+    class CQueue : CollectionBase
+    {
+        public void Enqueue(object item)
+        {
+            InnerList.Add(item);
+        }
+        public void Dequeue()
+        {
+            InnerList.RemoveAt(0);
+        }
+        public object Peekfront()
+        {
+            return InnerList[0];
+        }
+    }
+}
