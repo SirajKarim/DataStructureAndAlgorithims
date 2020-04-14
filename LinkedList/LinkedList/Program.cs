fc394cdf2240fcc79712362cf451ba5bcb9fbe03
SirajKarim <msirajakarim437@gmail.com>
2020-04-14 13:12:21 +0500

    working on linkedlist

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class linkedList
+    {
+        class Node
+        {
+            public int data;
+            public Node link;
+        }
+        public Node head;
+        public Node tail;
+        public int size;
+        public void Add(int _data)
+        {
+            size++;
+            Node newnode = new Node();
+            newnode.data = _data;
+            if (head==null)
+            {
+                head= newnode;
+            }
+            else
+            {
+                tail.link = newnode;
+            }
+            tail = newnode;
+
+        }
+        public void printAll()
+        {
+            Node temp = head;
+            while (head!=null)
+            {
+                Console.WriteLine(temp.data);
+                tail = temp.link;
+            }
+        }
+    }
+}
