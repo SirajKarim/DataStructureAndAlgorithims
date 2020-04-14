73d99ab0503d965b41e0f26f9114d2b683eeffa0
SirajKarim <msirajakarim437@gmail.com>
2020-04-14 13:10:34 +0500

    working on linear search

---
+++ Linear Search/Linear Search/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter finding number");
+            int target=int.Parse(Console.ReadLine());
+            int[] arr = { 2,4,5,6,8,9,7,10};
+            linearSearch(arr,target);
+           
+        }
+        public static void linearSearch(int[]arr,int target)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == target)
+                {
+                    Console.WriteLine("Found");
+                }
+                if (arr[i]!=target)
+                {
+                    Console.WriteLine("Not Found");
+                }
+            }
+        }
+    }
+}
